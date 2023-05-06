046ae3f93cc5288a833662dcb9bdc09cb71b4484
Kif_Francis <kif.zig@gmail.com>
2023-05-06 12:02:45 -0500

    Add enableValidation and setEventListeners to validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,6 +1,31 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+function showInputError(formEl, inputEl, options) {
+    const errorMessageEl
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+    if(!inputEl.validity.valid) {
+        showInputError(formEl, inputEl, options);
+    } else {
+        hideInputError();
+    }
+}
+
+
+
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+    });
+  });
+}
+
 function enableValidation(options) {
   // This works the same as Array.from [... ] - spread operator - expects array or array like object - makes a copy
   const formEls = [...document.querySelectorAll(options.formSelector)];
@@ -8,6 +33,8 @@ function enableValidation(options) {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
     });
+
+    setEventListeners(formEl, options);
 
     // Look for all inputs inside of form
     // Loop through all the inputs to see if all are valid
@@ -24,11 +51,11 @@ function enableValidation(options) {
 
 const config = {
   formSelector: ".modal__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
 };
 
 enableValidation(config);
