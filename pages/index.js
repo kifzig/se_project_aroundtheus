ce6238d010acaa64db866ad7eb821b021b16bfe0
Kif_Francis <kif.zig@gmail.com>
2023-05-20 18:30:40 -0500

    Update image

--- pages/index.js
+++ pages/index.js
@@ -24,9 +24,15 @@ const initialCards = [
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
+  /*
   {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+  */
+  {
+    name: "Cuyahoga Valley",
+    link: "https://images.unsplash.com/photo-1604448446634-6afe5d3882c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
 ];
 
