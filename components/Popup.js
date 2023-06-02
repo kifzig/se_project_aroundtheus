e7da181d3961985d594b0dc1d42aac465b66111e
Kif_Francis <kif.zig@gmail.com>
2023-06-01 23:28:20 -0500

    Add methods to Popup class

--- components/Popup.js
+++ components/Popup.js
@@ -1,21 +1,31 @@
 export default class Popup {
-  constructor({ popSelector }) {
+  // Many of these methods may be found in the utils.js
+  // You need to convert these over
+
+  constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
   }
 
   open() {
     // opens popup
+    this._popupElement.classList.add("modal_opened");
   }
 
   close() {
-    // closes popup
+    this._popupElement.classList.remove("modal_opened");
   }
 
-  _handleEscClose() {
-    // listens for esc button
+  _handleEscClose(evt) {
+    if (
+      evt.target.classList.contains(this._popupElement) ||
+      evt.target.classList.contains("modal__close-button")
+    ) {
+      this.close();
+    }
   }
 
   setEventListeners() {
-    // sets event listeners
+    // sets event listeners for close icon
+    this._popupElement.addEventListener("click", this._handleEscClose);
   }
 }
